e10ce5cad658f51f7ed9b46788e42119fa7dc361
Sandeepkondreddy <Sandeepkondreddy@gmail.com>
2017-10-05 05:39:51 +0530

    User Stage Validation added

--- www/js/Operations.js
+++ www/js/Operations.js
@@ -208,10 +208,31 @@ function clear()
 	txtOperation.value="";
 	hidNStageId.value="";
 }
+function qs() {
+    var query = window.location.search.substring(1);
+    var parms = query.split('&');
+    for (var i = 0; i < parms.length; i++) {
+        var pos = parms[i].indexOf('=');
+        if (pos > 0) {
+            var key = parms[i].substring(0, pos);
+            var val = parms[i].substring(pos + 1);
+            qsParm[key] = val;
+        }
+    }
 
-$(document).ready(function () {
-	$("#hidusrid").val(atob(qsParm["user"]));
-	alert($("#hidusrid").val());
+    if (parms.length > 0 && query != "") {
+        $("#hidusrid").val(atob(qsParm["user"]));
+        return true;
+    }
+    else {
+        window.location.href = 'Login.html';
+        return false;
+    }
+}
+$(document).ready(function () {debugger;
+	qs();
+	
+	//alert($("#hidusrid").val());
 		clear();
 	
 	    $("#imgScanTag").click(function () {

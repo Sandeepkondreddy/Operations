82cc2c383883452430c5bb0218aae8e4f4ae96a8
Sandeepkondreddy <Sandeepkondreddy@gmail.com>
2017-10-12 10:18:37 +0530

    Home Page Modified

--- www/js/home.js
+++ www/js/home.js
@@ -2,52 +2,95 @@ var qsParm = new Array();
 document.addEventListener("deviceready", onDeviceReady, false);
 
 function onDeviceReady() {
-   document.addEventListener("backbutton", onBackKeyDown, false);
-    $("#hidUUId").val(device.uuid);
+    document.addEventListener("backbutton", onBackKeyDown, false);
+    $("#hiduuid").val(device.uuid);
     window.plugins.imeiplugin.getImei(callback);
 }
 function callback(imei) {
-    $("#hidIMEI").val(imei);
+    $("#hidimei").val(imei);
 }
 function onBackKeyDown() {
-    }
-//var qsParm = new Array();
-function qs() {
-        var query = window.location.search.substring(1);
-        var parms = query.split('&');
-        for (var i = 0; i < parms.length; i++) {
-            var pos = parms[i].indexOf('=');
-            if (pos > 0) {
-                var key = parms[i].substring(0, pos);
-                var val = parms[i].substring(pos + 1);
-                qsParm[key] = val;
-            }
-        }
-        if (parms.length > 0) {
-            $("#hidusrid").val(atob(qsParm["user"]));
-            return true;
-        }
-        else {
-            window.location.href = 'Login.html';
-            return false;
-        }
 }
+function qs() {ValidateDevice();
+    var query = window.location.search.substring(1);
+    var parms = query.split('&');
+    for (var i = 0; i < parms.length; i++) {
+        var pos = parms[i].indexOf('=');
+        if (pos > 0) {
+            var key = parms[i].substring(0, pos);
+            var val = parms[i].substring(pos + 1);
+            qsParm[key] = val;
+        }
+    }
+    if (parms.length > 0) {
+        $("#hidusrid").val(atob(qsParm["user"]));
+        return true;
+    }
+    else {
+        window.location.href = 'Login.html';
+        return false;
+    }
+}
+$(document).ready(function () {
+    $("#loading").hide();
+    qs();
+    //GetDeviceStatus();
 
-$(document).ready(function(){
-        qs();		
-        $("#loading").hide();
-		
-        $(".box7").click(function(){
-			
+    $("#home").click(function () {
+        $.ajax({
+            type: "GET",
+			url: "http://apps.kpcl.com/KPCLOpsAPI/api/User/GetUserScreens/" + $("#hidusrid").val(),
+            //url: "http://202.83.27.199/KPCTSDS/api/Account/GetUserScreens/" + $("#hidusrid").val(),
+	        //url: "http://182.72.244.25/KPCTSDS/api/Account/GetUserScreens/" + $("#hidusrid").val(),
+            data: '{}',
+            contentType: "application/json",
+            success: function(result) {
+                window.location.href = result + '?user=' + btoa($("#hidusrid").val());
+            }
+        });
+    });
+
+	$(".box7").click(function(){
             $("#loading").show();
             window.location.href = 'Operations.html?user=' + btoa($("#hidusrid").val()) + '';
         });
+	
+    /* $("#btnSubmit").click(function (){
+
+            $(this).find("i.fa").attr('class', 'fa fa-spinner fa-spin');
+            $(this).find("span").text(" device is registering please wait...");
+            $(this).attr('disabled', true);
+            $(this).attr('class', 'btn btn-custom-icon');
+            $("#loading").show();
+            var Adddata = {};
+            Adddata.IMEI = $("#hidimei").val();
+            Adddata.UUID = $("#hiduuid").val();            
+            Adddata.User = $("#hidusrid").val();
+            $.ajax({
+                type: 'POST',
+                url: 'http://apps.kpcl.com/KPCLOpsAPI/api/Device/RegisterDevice',
+                dataType: "json",
+                data: Adddata,
+                success: function (loctyperesult) {
+                    alert('Device Registered Successfully');
+                },
+                error: function (xhr, status, error) {
+                    $("#btnSubmit").prop('disabled', false);
+                    alert('Error Occurred while Registring device.\n\r' + xhr.responseText);
+                }
+            });      
+        $(this).find("i.fa").attr('class', 'fa fa-check');
+        $(this).find("span").text(" Submit");
+        $(this).attr('disabled', false);
+        $(this).attr('class', 'btn btn-custom');
+        $("#loading").hide();
+    }); */
 });
 
 function ValidateDevice(){
     var Adddata = {};
-    Adddata.IMEI = $("#hidIMEI").val();
-    Adddata.UUID = $("#hidUUId").val();
+    Adddata.IMEI = $("#hidimei").val();
+    Adddata.UUID = $("#hiduuid").val();
     $.ajax({
         type: "POST",
         url: "http://apps.kpcl.com/KPCLOpsAPI/api/Device/ValidateDevice",
@@ -59,7 +102,7 @@ function ValidateDevice(){
                 alert('Device Already Registered.');
             }
             else {
-				alert('Device Not Registered, Please Contact IT Team.');
+				alert('Device Not Registered, Please contact IT Team.');
             } 
         },
         error: function () {
